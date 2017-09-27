300114487ff957d5836bf6470001e517c74404ea
vina aboshaira <nivin.aboshaira@gmail.com>
2017-09-27 18:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function logShout (string) {
 function logWhisper (string) {
   console.log('hello')
 }
-function sayHiToGrandma (string) {
-  console.log('hello')
+function sayHiToGrandma ('hello') {
   return ('I can't hear you!')
 }
