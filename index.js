20aa8c3d33a4685e376783ed69e39419b03b2ba0
vina aboshaira <nivin.aboshaira@gmail.com>
2017-09-27 18:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function whisper (string) {
   return string.toLowerCase()
 }
 function logShout (string) {
-  return string.toLowerCase()
+  console.log(logshout)
+  return ('hello').toUpperCase()
 }
